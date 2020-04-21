5b0f59daba7e539c2d7a1764260e711b42a86e27
lijinfeng <956654029@qq.com>
2020-04-21 15:34:15 +0800

    4.21

---
+++ src/Common/common.go
@@ -0,0 +1,14 @@
+package common
+
+func Yunsuan(a int,b int ,fuhao string)int  {
+	
+	var res int 
+	switch fuhao {
+		case "+": res = a+b 
+		case "-": res = a-b 
+		case "*": res = a*b 
+		case "/": res = a/b 
+		
+	}
+	return res
+}
