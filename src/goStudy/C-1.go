af00c268c1b60c4f4bacd3238a225785722f32b3
congyukun <15313922265@163.com>
2020-04-22 18:31:49 +0800

    匿名函数学完

--- src/goStudy/C-1.go
+++ src/goStudy/C-1.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+var ( //全局匿名函数
+	Fun = func(n1 int,n2 int) int{
+		return  n1*n2
+	}
+)
 func main() {
+	r := Fun(1,2)
+	fmt.Println(r)
 	//var c = getResult //函数作为参数
 	//fmt.Printf("c的类型%T","getsum的类型是%T",c,getSum)
 	//d := c(100,100)
 	//fmt.Println(d)
-	sum := sum(1, 2, 3, 3, 4, 4, 423, 21, 22)
+	//sum := sum(1, 2, 3, 3, 4, 4, 423, 21, 22)
+	//fmt.Println(sum)
 
-	fmt.Println(sum)
+	res := func(n1 int,n2 int) int{
+		return n1+n2
+	}
+	fmt.Println(res(11,22))
 }
 
 //测试函数作为参数
@@ -40,3 +51,10 @@ func sum(n1 int, n2 int, args ...int) int {
 	}
 	return sum
 }
+
+
+//init函数试先执行相当于其他语言的析构函数
+func init(){
+	fmt.Println("init........")
+}
+
