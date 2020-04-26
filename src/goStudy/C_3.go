ea1de0a938fdc899b28906bc925fe2cee61da23c
congyukun <15313922265@163.com>
2020-04-26 14:40:55 +0800

    更新

--- src/goStudy/C_3.go
+++ src/goStudy/C_3.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 	//字符串长度
-	strTest("sssss")
+	//strTest("sssss")
 	//字符串转整数
-	strTest("12")
+	//strTest("12")
+	//日期函数
+
+	//time := time.Now()
+
+	//fmt.Println(time.YearDay())
+	//fmt.Println(time.Format("2020/04/24 17:00:00"))
+
+	//捕获异常
+	//testRecover()
+//自定义输出错误信息 panic
+	testPanic()
+
 }
 
 func strTest(str string) {
@@ -25,4 +38,35 @@ func strTest(str string) {
 /*	s := strconv.Itoa(11)
 	fmt.Printf("整数转字符串 s = %v,s的类型 %T", s,s)*/
 
+}
+
+func testRecover(){
+	defer func() {
+		err := recover()
+		if err != nil{
+			fmt.Println("err = ",err)
+		}
+	}()
+
+	num1 := 10
+	num2 := 0
+	num3 := num1/num2
+
+	fmt.Println(num3)
+}
+
+func readConf(name string) (err error) {
+	if name == "conf" {
+		return nil
+	}else{
+		return errors.New("自定义返回。。。")
+	}
+}
+
+func testPanic()  {
+	err := readConf("ss")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("继续执行")
 }
